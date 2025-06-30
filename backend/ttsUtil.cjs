7bae2e4c239c6f295379296c0d31856444214f97
Rastamannen <johan@laurelli.com>
2025-06-30 15:56:09 +0200

    Update ttsUtil.cjs

--- backend/ttsUtil.cjs
+++ backend/ttsUtil.cjs
@@ -1,18 +1,19 @@
-import textToSpeech from '@google-cloud/text-to-speech';
-import dotenv from 'dotenv';
-dotenv.config();
+const textToSpeech = require('@google-cloud/text-to-speech');
+require('dotenv').config();
 
 const client = new textToSpeech.TextToSpeechClient();
 
-export async function ttsAudio(text, languageCode = "pt-PT") {
+async function ttsAudio(text, languageCode = "pt-PT") {
   const request = {
     input: { text },
     voice: { languageCode, ssmlGender: 'FEMALE' },
     audioConfig: { 
       audioEncoding: 'MP3',
-      speakingRate: 0.75 // Lägre = långsammare
+      speakingRate: 0.85 // långsammare tal
     }
   };
   const [response] = await client.synthesizeSpeech(request);
   return response.audioContent;
 }
+
+module.exports = { ttsAudio };
